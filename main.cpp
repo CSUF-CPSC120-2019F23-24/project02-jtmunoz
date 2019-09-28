b0971bab82e6761e3b9306c99a028ceb7cea738a
jtmunoz <jordantmunoz@gmail.com>
2019-09-28 15:11:15 -0700

    completes project 2

--- main.cpp
+++ main.cpp
@@ -1,19 +1,24 @@
-/ Name: Jordan Munoz
+// Name: Jordan Munoz
 // This program calculates and displays business expenses.
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <sstream>
 
 int main() {
-  int length_of_stay;
-  double stay_total, food_total;
+
   std::string location;
+  int length_of_stay;
+  double hotel, meal;
+  std::stringstream sshotel, ssmeal, sstotal;
+
+
 
   std::cout << "Welcome to Vacation Tracker.\n";
 
   //ask user for location
   std::cout << "Where did you stay? ";
-  std::cin  >> location;
+  getline(std::cin, location);
 
   //ask user for length of stay
   std::cout << "How many nights did you stay? ";
@@ -21,17 +26,21 @@ int main() {
 
   //ask user for total for stay charges
   std::cout << "What was the total for your hotel expenses? $";
-  std::cin  >> stay_total;
+  std::cin  >> hotel;
 
   //ask user for total of food charges
   std::cout << "What was the total of your food charges? $";
-  std::cin  >> food_total;
+  std::cin  >> meal;
+
+
+  double total = hotel + meal;
+  sshotel << std::setprecision(2) << std::fixed << "$" << hotel;
+  ssmeal << std::setprecision(2) << std::fixed << "$" << meal;
+  sstotal << std::setprecision(2) << std::fixed << "$" << total;
 
   // print out results
-  std::cout << std::setw(18) << "Location Visited" << std::setw(18) << "Nights Stayed"
-      << std::setw(18) << "Hotel Expenses" << std::setw(18) << "Food Expenses" << std::endl;
-  std::cout << std::setw(18) << location << std::setw(18) << length_of_stay
-      << std::setw(15) << "$" << stay_total << std::setw(15) << "$" << food_total << std::endl;
+  std::cout << std::setw(20) << "Location" << std::setw(20) << "Days" << std::setw(20) << "Hotel" << std::setw(20) << "Meal" << std::setw(20) << "Total" << std::endl;
+  std::cout << std::setw(20) << location.substr(0,18) << std::setw(20) << length_of_stay << std::setw(20) << sshotel.str() << std::setw(20) << ssmeal.str() << std::setw(20) << sstotal.str() << std::endl;
 
   return 0;
 }
